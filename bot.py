262f0d24235b855c65dd974ec27dc8894cfb2590
Lokkel <hereygersmilsn@gmail.com>
2021-03-24 14:45:14 +0100

    adding a few functions like joined

--- bot.py
+++ bot.py
@@ -16,6 +16,8 @@ bot = commands.Bot(command_prefix='&')
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
+#random qouotes
+
 @bot.command(name='kurwa', help='RAndom Lokkel quote')
 async def nine_nine(ctx):
 
@@ -31,6 +33,8 @@ async def nine_nine(ctx):
     response = random.choice(quotes)
     await ctx.send(response)
 
+#roll dice
+
 @bot.command(name='roll_dice', help='Simulates rolling dice. &roll_dice "number of dice"+"number of sides"')
 async def roll(ctx, number_of_dice: int, number_of_sides: int):
 
@@ -39,6 +43,8 @@ async def roll(ctx, number_of_dice: int, number_of_sides: int):
         for _ in range(number_of_dice)
     ]
     await ctx.send(', '.join(dice))
+
+#create channel
 
 @bot.command(name='create-channel', help='command + txt channel name')
 @commands.has_role('staff')
@@ -49,10 +55,29 @@ async def create_channel(ctx, channel_name='snekfun'):
         print(f'Creating a new channel: {channel_name}')
         await guild.create_text_channel(channel_name)
 
+#errorr
+
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CheckFailure):
         await ctx.send('You do not have the correct role for this command.')
+
+
+
+#reapeter
+
+@bot.command(name='repeat')
+async def repeat(ctx, times: int, content='repeating...'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+#joined
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
 
 
 bot.run(TOKEN)
